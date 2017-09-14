a6d9db76794a5cac80fa8ed26c89d54468acf678
Olga Starkova <olstar.ru@gmail.com>
2017-09-14 19:32:30 +0200

    removed config from initializer

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,15 +7,4 @@
 
 
 
-# Added following this warning:
-# DEPRECATION WARNING: Time columns will become time zone aware in Rails 5.1.
-# This still causes `String`s to be parsed as if they were in `Time.zone`,and `Time`s to be converted to `Time.zone`.
 
-# To keep the old behavior, you must add the following to your initializer:
-# config.active_record.time_zone_aware_types = [:datetime]
-
-# To silence this deprecation warning, add the following:
-# config.active_record.time_zone_aware_types = [:datetime, :time]
-
-
-config.active_record.time_zone_aware_types = [:datetime]
